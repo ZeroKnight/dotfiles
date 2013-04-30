c8513cf803935378e7b75d265a7fcf80d252d378
ZeroKnight <xzeroknightx@gmail.com>
2013-04-29 22:09:41 -0400

    Don't need that cpanm alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -8,7 +8,6 @@ alias iptraf='sudo iptraf-ng'
 alias untar='tar -xvf'
 alias scrot="scrot 'SCREENSHOT_\%m-\%d-\%Y_\%H:\%M:\%S.png' -d3 -q100 -ms -e 'mv \$f ~/Pictures/Screenshots/'"
 alias reloadtint2='killall -SIGUSR1 tint2'
-alias cpanm='cpanm --sudo --skip-installed'
 alias gproc='ps aux | grep -i'
 
 ### Tricks and 'new' Commands #####
