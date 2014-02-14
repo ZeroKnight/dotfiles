402012c35c1103ee9b2e908512927f4aeab4876c
ZeroKnight <xzeroknightx@gmail.com>
2014-02-14 01:10:18 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # "Installer" for ZeroKnight's dotfiles
 # Intended to be run via:
-#   curl -Ss https://github.com/ZeroKnight/dotfiles/install.sh | sh
+#   curl -L https://raw.github.com/ZeroKnight/dotfiles/master/install.sh | sh
 
 DOTDIRS="\
 .config/openbox
@@ -18,45 +18,58 @@ DOTFILES="\
 .gvimrc
 .screenrc
 .vimrc
-.vimrc_funcitons
-.xinitc
+.xinitrc
 .zshrc
 .zprofile"
 
-cd $HOME
+# Splash
+echo
+echo "*=================================*"
+echo "| ZeroKnight's Dotfiles Installer |"
+echo "*=================================*"
+echo
+sleep 1
 
-### Splash ########################
-echo -e "\n--- Grabbing ZeroKnight's Dotfiles... ---\n"
+# Clone dotfiles repo
+if [[ -d $HOME/dotfiles && -d $HOME/dotfiles/.git ]]; then
+    echo "[!!] Dotfiles already cloned! Skipping..."
+elif [[ -d $HOME/dotfiles && ! -d $HOME/dotfiles/.git ]]; then
+    echo "[**] 'dotfiles' directory exists, but is not a git repository. Aborting."
+    exit 1
+else
+    git clone git://github.com/ZeroKnight/dotfiles.git dotfiles
+fi
 
-### Clone the Repo ################
-git clone git://github.com/ZeroKnight/dotfiles.git dotfiles
-
-### Backup existing dotfiles ######
-### Create symlinks ###############
-echo -e "\n--- Symlinking Dotdirs... ---\n"
-for d in $DOTDIRS; do
-    if [ -d $d ]; then
-        mv $d ${d}.nongit
-        echo "[!] $d backed up as ${d}.nongit"
-    fi
-
-    ln -s ${HOME}/dotfiles/$d ${HOME}/$d
-done
-
-echo -e "\n--- Symlinking Dotfiles... ---\n"
-for f in $DOTFILES; do
-    if [ -e $f ]; then
-        mv $f ${f}.nongit
-        echo "[!] $f backed up as ${f}.nongit"
-    fi
-
-    ln -s ${HOME}/dotfiles/$f ${HOME}/$f
-done
-
-### Update submodules #############
-echo -e "\n--- Updating submodules... ---\n"
+# Initialize submodules
+echo "[--] Initializing submodules..."
 git --git-dir="dotfiles" submodule init
 git --git-dir="dotfiles" submodule update
 
+# Install oh-my-zsh!
+echo "[--] Installing oh-my-zsh!..."
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# Create symlinks in HOME, backing up existing files
+echo "[--] Symlinking directories..."
+for d in $DOTDIRS; do
+    if [ -d $d ]; then
+        mv $d ${d}.nongit
+        echo "[!!] $d backed up as ${d}.nongit"
+    fi
+    ln -s $HOME/dotfiles/$d $HOME/$d
+done
+
+echo "[--] Symlinking files..."
+for f in $DOTFILES; do
+    if [ -e $f ]; then
+        mv $f ${f}.nongit
+        echo "[!!] $f backed up as ${f}.nongit"
+    fi
+    ln -s $HOME/dotfiles/$f $HOME/$f
+done
+
 ### Farewell! #####################
-echo -e "\n--- Dotfiles Installation done! ---"
+echo "[--] Dotfiles installation done!"
+echo "[--] Don't forget to grab the private keys!"
+exit 0
+
