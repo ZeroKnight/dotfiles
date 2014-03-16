fa91be91f3c2312e82baa01caf1df2d02655f0e2
ZeroKnight <xzeroknightx@gmail.com>
2014-03-16 03:18:24 -0400

    zsh: Fixed and tweaked 'extract' plugin

--- .oh-my-zsh/custom/plugins/extract/extract.plugin.zsh
+++ .oh-my-zsh/custom/plugins/extract/extract.plugin.zsh
@@ -5,30 +5,41 @@
 # http://github.com/ZeroKnight/dotfiles
 
 extract () {
-    if [ -f $1 ] ; then
-        case $1 in
-            *.tar.bz2)  tar xjf $1      ;;
-            *.tar.gz)   tar xzf $1      ;;
-            *.bz2)      bunzip2 $1      ;;
-            *.rar)      unrar x $1      ;;
-            *.gz)       gunzip $1       ;;
-            *.tar)      tar xf $1       ;;
-            *.tbz2)     tar xjf $1      ;;
-            *.tgz)      tar xzf $1      ;;
-            *.zip)      unzip $1        ;;
-            *.Z)        uncompress $1   ;;
-            *) # Fallback
-                case file -bi $1 | grep -Eo '(x-\w+(-\w+)?|zip)' in
-                    x-bzip2)            tar xjf $1 || bunzip2 $1 ;;
-                    x-gzip)             tar xzf $1 || gunzip $1 ;;
-                    x-tar)              tar xf $1 ;;
-                    x-rar-compressed)   unrar x $1 ;;
-                    zip)                unzip $1 ;;
-                    *) echo "'$1' cannot be extracted via extract()" ;;
-                esac
-        esac
-    else
-        echo "'$1' is not a valid file"
+    if [[ $# < 1 ]]; then
+        echo "Usage: $0 <files...>"
+        return 2
     fi
+
+    for f in $@; do
+        if [ -f $1 ] ; then
+            case $1 in
+                *.tar.bz2)  tar xjf $1      ;;
+                *.tar.gz)   tar xzf $1      ;;
+                *.bz2)      bunzip2 $1      ;;
+                *.rar)      unrar x $1      ;;
+                *.gz)       gunzip $1       ;;
+                *.tar)      tar xf $1       ;;
+                *.tbz2)     tar xjf $1      ;;
+                *.tgz)      tar xzf $1      ;;
+                *.zip)      unzip $1        ;;
+                *.Z)        uncompress $1   ;;
+                *) # Fallback
+                    case $(file -bi $1 | grep -Eo '(x-\w+(-\w+)?|zip)') in
+                        x-bzip2)            tar xjf $1 || bunzip2 $1 ;;
+                        x-gzip)             tar xzf $1 || gunzip $1 ;;
+                        x-tar)              tar xf $1 ;;
+                        x-rar-compressed)   unrar x $1 ;;
+                        zip)                unzip $1 ;;
+                        *)
+                            echo "'$1' cannot be extracted via $0"
+                            return 1
+                        ;;
+                    esac
+            esac
+        else
+            echo "'$1' is not a valid file"
+            return 3
+        fi
+    done
 }
 
