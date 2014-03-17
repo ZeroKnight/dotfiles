bd8871146917a600566bfb3ec6528b711e3a462e
ZeroKnight <xzeroknightx@gmail.com>
2014-03-17 02:46:38 -0400

    zsh: Fix 'fasd' plugin
    Fixed feeding of $fasd_hook to `fasd --init`
    Tweaked and added fasd aliases

--- .oh-my-zsh/custom/plugins/fasd/fasd.plugin.zsh
+++ .oh-my-zsh/custom/plugins/fasd/fasd.plugin.zsh
@@ -6,11 +6,12 @@ local fasd_hook='zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-instal
 
 if [ $(command -v fasd) ]; then # check if fasd is installed
     if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
-        fasd --init "$fasd_hook" >| "$fasd_cache"
+        fasd --init ${=fasd_hook} >| "$fasd_cache"
     fi
     source "$fasd_cache"
 
     # Set aliases
-    alias v='f -e vim'
+    alias v='f -e $EDITOR' # EDITOR will always be either 'vim' or 'gvim'
+    alias l='f -e less'
 fi
 
