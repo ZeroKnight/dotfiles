7bbd14f9af679bf3f55a3fdca5ba3a6cd5042224
ZeroKnight <xzeroknightx@gmail.com>
2015-01-30 03:32:12 -0500

    Add custom reboot function

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -6,6 +6,28 @@ psg() {
     ps -fp $(pgrep $@)
 }
 
+reboot() {
+    local lin=0
+    local win=2
+    # TODO: Check privileges. Check for things like sudo groups where this
+    # command is allowed to be used without root
+    if [ $# -gt 0 ]; then
+        case $1 in
+            lin )
+                sudo grub-reboot $lin
+            ;;
+            win )
+                sudo grub-reboot $win
+            ;;
+            * )
+                echo "Unknown boot target $1"
+                exit 1
+            ;;
+        esac
+    fi
+    sudo reboot
+}
+
 vman() {
     vim -c "Man $* | only"
 
