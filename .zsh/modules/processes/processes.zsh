b4580f5bfbff3d998eb5763c20b7fd50838d6857
ZeroKnight <xzeroknightx@gmail.com>
2016-10-24 03:13:45 -0700

    zsh: Update processes module
      - Refactored aliases, they're less terrible
      - Make use of PS_FORMAT instead of relying on $ps_opts
      - Made psg() autoloadable since we're not relying on $ps_opts anymore

--- .zsh/modules/processes/processes.zsh
+++ .zsh/modules/processes/processes.zsh
@@ -1,21 +1,10 @@
 # ps Aliases and Shortcuts
 
-# "Default" options to use for standard ps alias
-ps_opts='-efly'
+export PS_PERSONALITY='linux'
+export PS_FORMAT='stat,euser:12,pid,ppid,time=CPU_TIME,nlwp,ni,%cpu,%mem,rss,vsz=VRSS,stime,tty=TTY,cmd=COMMAND'
 
-# Like `ps | grep foo`, but shorter and less ugly
-# eval expands $ps_opts
-eval "psg() {
-  local pids=\"\$(pgrep -d, \$1)\"
-  [ -n \"\$pids\" ] || return 1
-  command ps ${ps_opts/e/} -p \"\$pids\"
-}"
+alias ps='ps -e'
+alias psf='ps --forest'
+alias psm='ps --sort=-rss'
+alias psc='ps --sort=-%cpu'
 
-alias ps="ps $ps_opts | less"
-alias psf='\ps -elfy --forest | less'
-alias psv='\ps --forest -eo uid,pid,ppid,c,pcpu,pmem,rsz,psr,stime,time,tty,cmd | less'
-alias psm='\ps -eo rss,cmd | sort -b -nr | less'
-alias psc='\ps -eo pcpu,c,nice,stat,time,cmd | sort -k1,1r | less'
-
-# Clean up
-unset ps_opts
