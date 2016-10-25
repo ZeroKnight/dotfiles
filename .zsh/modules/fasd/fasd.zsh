c19180d9fc3ffa7c398ba43ab85122458acb1551
ZeroKnight <xzeroknightx@gmail.com>
2016-10-25 03:00:56 -0700

    zsh: [fasd] Fix vars and zcompile fasd cache

--- .zsh/modules/fasd/fasd.zsh
+++ .zsh/modules/fasd/fasd.zsh
@@ -5,7 +5,7 @@
 (( $+commands[fasd] )) || return 1
 
 # _fasd_config="${XDG_CONFIG_HOME:-"$HOME/.config"}/fasd/config"
-_FASD_DATA="${XDG_CACHE_HOME:-"$HOME/.cache"}/fasd/fasd"
+export _FASD_DATA="${XDG_CACHE_HOME:-"$HOME/.cache"}/fasd/fasd"
 _fasd_cache="${XDG_CACHE_HOME:-"$HOME/.cache"}/fasd/init"
 _fasd_hooks="$(print zsh-{c,w}comp{,-install})"
 
@@ -17,9 +17,11 @@ for dir ($_FASD_DATA $_fasd_cache) {
 
 # Cache initialization code if needed
 if [[ "$commands[fasd]" -nt "$_fasd_cache" || ! -s "$_fasd_cache" ]]; then
-  fasd --init ${=_fasd_hooks} >| "$_fasd_cache"
+  fasd --init ${=_fasd_hooks} >| $_fasd_cache
+  zcompile $_fasd_cache
 fi
 source "$_fasd_cache"
+unset _fasd_{cache,hooks}
 
 ### Aliases
 
