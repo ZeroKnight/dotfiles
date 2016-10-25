e67806b9cbf75a23e1ebef8e1ab304f75fc51c5b
ZeroKnight <xzeroknightx@gmail.com>
2016-10-25 02:31:34 -0700

    zsh: Add fasd module
      - _fasd_preexec() and fasd_cd are autoloadable functions
      - Backported some PRs from clvv/fasd
        - [clvv/fasd#54] Correction of newline print statement in fasd_cd
        - [clvv/fasd#75] fasd_cd: return 1 in case no dir was found
        - *Partial* [clvv/fasd#77] XDG Compliance
        - [clvv/fasd#99] Do not "eval" in zsh's _fasd_preexec

---
+++ .zsh/modules/fasd/fasd.zsh
@@ -0,0 +1,38 @@
+#
+# Fasd Configuration
+#
+
+(( $+commands[fasd] )) || return 1
+
+# _fasd_config="${XDG_CONFIG_HOME:-"$HOME/.config"}/fasd/config"
+_FASD_DATA="${XDG_CACHE_HOME:-"$HOME/.cache"}/fasd/fasd"
+_fasd_cache="${XDG_CACHE_HOME:-"$HOME/.cache"}/fasd/init"
+_fasd_hooks="$(print zsh-{c,w}comp{,-install})"
+
+### Initialize fasd
+
+for dir ($_FASD_DATA $_fasd_cache) {
+  [[ -d ${dir:h} ]] || mkdir -p ${dir:h}
+}
+
+# Cache initialization code if needed
+if [[ "$commands[fasd]" -nt "$_fasd_cache" || ! -s "$_fasd_cache" ]]; then
+  fasd --init ${=_fasd_hooks} >| "$_fasd_cache"
+fi
+source "$_fasd_cache"
+
+### Aliases
+
+# Standard
+alias a='fasd -a'
+alias s='fasd -si'
+alias sd='fasd -sid'
+alias sf='fasd -sif'
+alias d='fasd -d'
+alias f='fasd -f'
+alias z='fasd_cd -d'
+alias zz='fasd_cd -d -i'
+
+# Extra
+alias v='f -e $EDITOR'
+
