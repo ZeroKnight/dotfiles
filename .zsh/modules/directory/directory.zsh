8eb0adc8f5d01a739e88012c8c82a3bbf1db5c38
ZeroKnight <xzeroknightx@gmail.com>
2017-09-27 02:12:30 -0700

    zsh: Update LS_COLORS

--- .zsh/modules/directory/directory.zsh
+++ .zsh/modules/directory/directory.zsh
@@ -5,7 +5,7 @@
 ### dircolors
 
 # Set LS_COLORS
-eval $(dircolors -b $HOME/.config/.dircolors)
+[[ -e $HOME/.config/.dircolors ]] && eval $(dircolors -b $HOME/.config/.dircolors)
 
 # Enable dircolors in file completion
 zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
