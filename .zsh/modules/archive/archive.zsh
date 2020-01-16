304349d234763f6f64bd0f87e31e220cdf340514
ZeroKnight <xzeroknightx@gmail.com>
2020-01-15 18:50:39 -0800

    zsh: Fix test syntax

--- .zsh/modules/archive/archive.zsh
+++ .zsh/modules/archive/archive.zsh
@@ -6,6 +6,6 @@
 
 # Use the parallelized versions of gzip and bzip2 as compression times are
 # significantly faster
-[[ $+commands[pigz] ]] && alias gzip='pigz' gunzip='unpigz'
-[[ $+commands[pbzip2] ]] && alias bzip2='pbzip2' bunzip2='pbunzip2'
+(( $+commands[pigz] )) && alias gzip='pigz' gunzip='unpigz'
+(( $+commands[pbzip2] )) && alias bzip2='pbzip2' bunzip2='pbunzip2'
 
