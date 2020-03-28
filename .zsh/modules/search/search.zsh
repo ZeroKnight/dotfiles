c80d36ac07c4274bf4ccce46f06165f62252cd0e
ZeroKnight <xzeroknightx@gmail.com>
2020-03-28 01:56:50 -0700

    zsh: More fzf stuff

--- .zsh/modules/search/search.zsh
+++ .zsh/modules/search/search.zsh
@@ -7,3 +7,9 @@ alias grep='grep --color=auto'
 
 # ripgrep
 export RIPGREP_CONFIG_PATH=$HOME/.config/ripgrep
+
+# fzf
+if (( $+commands[fzf] )); then
+  export FZF_DEFAULT_COMMAND='fd --type file --color=always --follow --hidden --exclude .git'
+  export FZF_DEFAULT_OPTS='--height 40%'
+fi
