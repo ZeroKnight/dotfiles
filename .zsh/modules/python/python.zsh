da794d013b58293ee15257dfa70da33016091cbb
ZeroKnight <xzeroknightx@gmail.com>
2020-12-18 18:52:33 -0800

    zsh: Automatic python virtualenv activation

--- .zsh/modules/python/python.zsh
+++ .zsh/modules/python/python.zsh
@@ -2,6 +2,12 @@
 # Python settings and aliases et al
 #
 
+# venv directory names for auto-detection
+typeset -gT _PYTHON_VENV_NAMES python_venv_names=(venv .venv)
+
+autoload -Uz add-zsh-hook
+add-zsh-hook chpwd chpwd_python_venv
+
 # Prefer python3, even on systems that default to 2.7
 alias python='python3'
 alias py='python'
@@ -9,3 +15,4 @@ alias pip='pip3'
 alias pydoc='pydoc3'
 
 alias ipy='ipython'
+alias venv='python3 -m venv venv'
