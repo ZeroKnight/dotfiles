9297ae220d226a062fb29bad93e4711ce2fd6999
ZeroKnight <xzeroknightx@gmail.com>
2021-05-04 01:22:20 -0700

    zsh: Fix FZF not handling ANSI escapes
    Why is this not default?

--- .zsh/modules/search/search.zsh
+++ .zsh/modules/search/search.zsh
@@ -11,5 +11,5 @@ export RIPGREP_CONFIG_PATH=$HOME/.config/ripgrep
 # fzf
 if (( $+commands[fzf] )); then
   export FZF_DEFAULT_COMMAND='fd --type file --color=always --follow --hidden --exclude .git'
-  export FZF_DEFAULT_OPTS='--height 40%'
+  export FZF_DEFAULT_OPTS='--ansi --height 40%'
 fi
