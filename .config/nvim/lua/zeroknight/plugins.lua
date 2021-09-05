4353e4898574b4b6e41b99b8d6b5ee09a57da654
ZeroKnight <xzeroknightx@gmail.com>
2021-09-05 16:11:10 -0700

    nvim: Lazy-load more plugins
    packer offers even more lazy-loading options than vim-plug did, so let's
    take advantage of them. I also simply seemed to have forgotten to port
    over lazy-loading that I *did* have in place with vim-plug...

--- .config/nvim/lua/zeroknight/plugins.lua
+++ .config/nvim/lua/zeroknight/plugins.lua
@@ -11,7 +11,6 @@ return require('packer').startup{
     use 'wbthomason/packer.nvim'
 
     -- UI Plugins {{{1
-    use 'airblade/vim-gitgutter'  -- TODO: try out lewis6991/gitsigns.nvim
     use 'justinmk/vim-dirvish'
     use 'mhinz/vim-startify'
     use 'kshenoy/vim-signature'
@@ -28,6 +27,7 @@ return require('packer').startup{
     }
     use {
       'mengelbrecht/lightline-bufferline',
+      requires = 'itchyny/lightline.vim',
       setup = function()
         local function set(var, val) vim.g['lightline#bufferline#' .. var] = val end
         set('right_aligned', 1)
@@ -38,7 +38,7 @@ return require('packer').startup{
 
     use {
       'nvim-telescope/telescope.nvim',
-      requires = {'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim'},
+      requires = {'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim', 'kyazdani42/nvim-web-devicons'},
       config = [[require('plugin.telescope')]]
     }
 
@@ -49,6 +49,7 @@ return require('packer').startup{
 
     use {
       'folke/zen-mode.nvim',
+      cmd = 'ZenMode',
       config = function()
         require('zen-mode').setup {
           window = {
@@ -104,18 +105,13 @@ return require('packer').startup{
     }
 
     -- Analysis and Debugging {{{1
-
-    use 'mhinz/vim-grepper'
+    use {'mhinz/vim-grepper', cmd = 'Grepper'}
     use 'romainl/vim-qlist'
 
-    -- TODO: pick one (or neither? maybe telescope or something can show LSP symbols)
-    -- tagbar doesn't support LSP, but vista has problems with ctags, and possibly only
-    -- supports an LSP tree view for coc?
-    use 'liuchengxu/vista.vim'
-    use 'majutsushi/tagbar'
+    -- TBD: can treesitter replace tagbar in some way?
+    use {'majutsushi/tagbar', cmd = {'Tagbar', 'TagbarOpen', 'TagbarToggle'}}
 
-    -- TBD: Do I still need ALE with LSP? I suppose there's certain linters
-    -- that would still be nice to have
+    -- TODO: replace ALE with nvim-lint or diagnostic-languageserver
     use 'dense-analysis/ale'
 
     -- Debug Adapter Protocol (DAP)
@@ -124,16 +120,23 @@ return require('packer').startup{
 
     -- Completion {{{1
     use {
+      -- NOTE: Compe has been deprecated (just as I started using it, go figure...).
+      -- Switch to hrsh7th/nvim-cmp once it's had time to mature.
       'hrsh7th/nvim-compe',
       after = 'lexima.vim',
       config = [[require('plugin.completion')]]
     }
 
     -- Editing {{{1
-    use 'chrisbra/NrrwRgn'  -- TODO: experiment with this and find good use cases
+    use {  -- TODO: experiment with this and find good use cases
+      'chrisbra/NrrwRgn',
+      cmd = {
+        'NR', 'NW', 'NRV', 'NUD', 'NRP', 'NRL', 'NarrowRegion', 'NarrowWindow', 'NRPrepare'
+      }
+    }
     use 'christoomey/vim-sort-motion'
     use 'godlygeek/tabular'
-    use 'reedes/vim-wordy'
+    use {'reedes/vim-wordy', cmd = 'Wordy'}
     use 'tpope/vim-abolish'
     use 'tpope/vim-commentary'
     use 'tpope/vim-surround'
@@ -186,18 +189,23 @@ return require('packer').startup{
       end
     }
 
+    -- Git Integration {{{1
+    use {
+      {'tpope/vim-fugitive'},
+      {'tpope/vim-rhubarb', requires = 'tpope/vim-fugitive'}
+    }
+    use 'airblade/vim-gitgutter'  -- TODO: try out lewis6991/gitsigns.nvim
+
     -- Utility Plugins {{{1
     use '~/Projects/vim-signjump'
     use 'Konfekt/FastFold'
-    use 'moll/vim-bbye'
-    use 'tpope/vim-characterize'
-    use 'tpope/vim-dispatch'
     use 'tpope/vim-eunuch'
-    use 'tpope/vim-fugitive'
-    use {'tpope/vim-rhubarb', requires = 'tpope/vim-fugitive'}
-    use 'tpope/vim-obsession'
     use 'tpope/vim-scriptease'
     use 'tpope/vim-unimpaired'
+    use {'moll/vim-bbye', cmd = {'Bdelete', 'Bwipeout'}}
+    use {'tpope/vim-characterize', keys = 'ga'}
+    use {'tpope/vim-dispatch', cmd = {'Dispatch', 'Make', 'FocusDispatch', 'Start'}}
+    use {'tpope/vim-obsession', cmd = 'Obsession'}
 
     -- Language Support {{{1
     -- TBD: Dump this and just grab what we care about; I'm not using the vast majority of filetypes
