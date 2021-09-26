ecd9727d247567289394bcf7af3c52e68190a152
ZeroKnight <xzeroknightx@gmail.com>
2021-09-26 01:53:25 -0700

    nvim: Update globals.lua
    - Guard `zeroknight` against reloads
    - Prefix names with `_G` to avoid 'lowercase global' linter wanrings
    - Make `dump` behave more like `put` in the nvim-lua-guide
      - Return the arguments given to `dump`
      - Delimit printed arguments with newlines instead of spaces

--- .config/nvim/lua/zeroknight/globals.lua
+++ .config/nvim/lua/zeroknight/globals.lua
@@ -1,29 +1,29 @@
 -- Assorted utility functions, optionally in global scope
 
 -- Namespace for my global variables
-zeroknight = {}
+_G.zeroknight = _G.zeroknight or {}
 
 -- Convenient shortcut for printing lua objects
-function dump(...)
-  local objects = vim.tbl_map(vim.inspect, {...})
-  print(unpack(objects))
+function _G.dump(...)
+  print(table.concat(vim.tbl_map(vim.inspect, {...}), '\n'))
+  return ...
 end
 
 -- Return a subpath under a standard path
-function as_stdpath(what, path)
+function _G.as_stdpath(what, path)
   return string.format('%s/%s', vim.fn.stdpath(what), path)
 end
 
 -- Lua shorthand for VimL has()
-function has(what)
+function _G.has(what)
   return vim.fn.has(what) == 1
 end
 
 -- Modded from tjdevries
 if pcall(require, "plenary") then
-  reload = require("plenary.reload").reload_module
+  _G.reload = require("plenary.reload").reload_module
 
-  rerequire = function(name)
+  _G.rerequire = function(name)
     reload(name)
     return require(name)
   end
