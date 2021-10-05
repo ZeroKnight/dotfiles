5d3df603aa80e9e4f39937d9d5e46053835e9078
ZeroKnight <xzeroknightx@gmail.com>
2021-10-04 21:28:27 -0700

    nvim: Factor out log methods to `zeroknight.util`

--- .config/nvim/lua/zeroknight/bootstrap.lua
+++ .config/nvim/lua/zeroknight/bootstrap.lua
@@ -1,59 +1,46 @@
 -- Initial Neovim setup
 -- Idea yoinked from tjdevries :)
 
-local msg_prefix = '[zeroknight.bootstrap]'
+local util = require('zeroknight.util')
 
-local function msg(...)
-  print(msg_prefix, ...)
-end
-
-local function warn(...)
-  local args = {string.format('%s WARNING: ', msg_prefix), ...}
-  vim.api.nvim_echo(
-    vim.tbl_map(function(x) return {x, 'WarningMsg'} end, args), true, {}
-  )
-end
-
-local function err(...)
-  error(string.format('%s %s', msg_prefix, table.concat({...}, ' ')))
-end
+local format = string.format
 
 local function download_packer()
-  local choice = string.lower(vim.fn.input(msg_prefix .. ' Download Packer? (y/n) ')) 
-  vim.cmd('redraw')
+  local choice = string.lower(vim.fn.input(format('[%s] Download Packer? (y/n) ', util.get_module_name())))
+  vim.cmd 'redraw'
   if choice ~= 'y' then
-    warn('Cannot load config until Packer is set up.')
+    util.warn('Cannot load config until Packer is set up.')
     vim.opt.loadplugins = false  -- Don't try to load our incomplete config
     return
   end
 
   local packer_url = 'https://github.com/wbthomason/packer.nvim'
-  local packer_dir = string.format('%s/site/pack/packer/start', vim.fn.stdpath('data'))
+  local packer_dir = format('%s/site/pack/packer/start', vim.fn.stdpath('data'))
 
   vim.fn.mkdir(packer_dir, 'p')
-  msg('Downloading Packer...')
-  msg(vim.fn.system(string.format('git clone --depth=1 %s %s/packer.nvim', packer_url, packer_dir)))
+  util.msg('Downloading Packer...')
+  util.msg(vim.fn.system(format('git clone --depth=1 %s %s/packer.nvim', packer_url, packer_dir)))
   if vim.g.shell_error then
-    err(string.format('Error downloading Packer:\n%s', vim.g.shell_error))
+    util.error(format('Failed to download Packer:\n%s', vim.g.shell_error))
   end
 end
 
 -- Set up Neovim state directories
 local function make_state_dirs()
-  msg('Creating Neovim state directories')
+  util.msg('Creating Neovim state directories')
   for _, dir in ipairs{'swap', 'undo', 'view', 'session'} do
-    local path = string.format('%s/%s', vim.fn.stdpath('data'), dir)
+    local path = format('%s/%s', vim.fn.stdpath('data'), dir)
     vim.fn.mkdir(path, 'p')
   end
 end
 
 -- Load packer and download/install plugins
 local function install_plugins()
-  msg('Installing plugins...')
+  util.msg('Installing plugins...')
   vim.cmd 'packloadall'
   local has_packer, packer = pcall(require, 'packer')
   if not has_packer then
-    err('Cannot install plugins, unable to require packer')
+    util.error('Cannot install plugins, unable to require packer')
     return
   end
   require('zeroknight.plugins')
@@ -65,14 +52,14 @@ return function()
     make_state_dirs()
     download_packer()
     install_plugins()
-    msg('Restart Neovim after packer sync to complete bootstrap.')
+    util.info('Restart Neovim after packer sync to complete bootstrap.')
     return true
   end
 
   -- Ensure that we have plenary
-  local has_plenary, Path = pcall(require, 'plenary.path')
+  local has_plenary = pcall(require, 'plenary.path')
   if not has_plenary then
-    err("Packer is installed, but plenary isn't available.")
+    util.error("Packer is installed, but plenary isn't available.")
     return true
   end
 
@@ -81,7 +68,7 @@ return function()
   local python_versions = {3}  -- TODO: For now, I only care about Python3
   for _, version in ipairs(python_versions) do
     if not providers.get_python_venv(version):exists() then
-      msg('Creating venv for Python', version, 'Provider')
+      util.msg('Creating venv for Python', version, 'Provider')
       if not providers.create_python_venv(version) then
         return true
       end
--- .config/nvim/lua/zeroknight/util/init.lua
+++ .config/nvim/lua/zeroknight/util/init.lua
@@ -1,13 +1,51 @@
 -- Various utility stuff
 
+local format = string.format
+
 local M = {}
+
+-- Returns the module name of the calling function
+function M.get_module_name()
+  local name
+  local info = debug.getinfo(3, 'S')
+  local filename = string.match(info.source, '^@(.+)$')
+  if filename ~= nil then
+    name = filename:match('lua/(%S+)%.lua$'):gsub('/', '.'):gsub('%.init$', '')
+  else
+    name = info.source
+  end
+  return name
+end
+
+function M.log(hl, ...)
+  local args = {format('[%s] ', M.get_module_name()), ...}
+  vim.api.nvim_echo(
+    vim.tbl_map(function(x) return {x, hl} end, args), true, {}
+  )
+end
+
+function M.msg(...)
+  M.log('Normal', ...)
+end
+
+function M.info(...)
+  M.log('DiagnosticInfo', 'Info: ', ...)
+end
+
+function M.warn(...)
+  M.log('DiagnosticWarn', 'Warning: ', ...)
+end
+
+function M.error(...)
+  M.log('DiagnosticError', 'Error: ', ...)
+end
 
 function M.t(key)
   return vim.api.nvim_replace_termcodes(key, true, true, true)
 end
 
 function M.cmdf(cmd, ...)
-  vim.cmd(string.format(cmd, ...))
+  vim.cmd(format(cmd, ...))
 end
 
 function M.python_interpreter()
@@ -21,7 +59,7 @@ end
 function M.python_version(interpreter)
   local ver_cmd = vim.call(
     'shellescape', "import sys; print('.'.join(map(str, sys.version_info[:2])), end='')")
-  return vim.call('system', string.format('%s -c %s', interpreter, ver_cmd))
+  return vim.call('system', format('%s -c %s', interpreter, ver_cmd))
 end
 
 return M
