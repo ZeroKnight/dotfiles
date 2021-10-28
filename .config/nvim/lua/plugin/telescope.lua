0670a0eafdd604299113b6f7f31948d7a733e3d2
ZeroKnight <xzeroknightx@gmail.com>
2021-10-28 02:48:18 -0700

    nvim: Fix `plugin.telescope`
    - Create a proper module table
    - Moved `extensions` sugar into the module's `__index` metamethod
    - Reorganized order of definitions
      - Define custom pickers before setting mappings so as not to attempt
        to create `nil` partials
      - General organizing

--- .config/nvim/lua/plugin/telescope.lua
+++ .config/nvim/lua/plugin/telescope.lua
@@ -16,7 +16,28 @@ local trouble = require("trouble.providers.telescope")
 
 local util = require('zeroknight.util')
 
-local M = {}
+local function open_in_file_browser(prompt_bufnr)
+  local entry = action_state.get_selected_entry()
+  local dir = from_entry.path(entry)
+  if vim.fn.isdirectory(dir) == 1 then
+    actions.close(prompt_bufnr)
+    builtin.file_browser{cwd = dir}
+  else
+    util.msg('Not a directory: ', dir)
+  end
+end
+
+local M = setmetatable({}, {
+    __index = function(_, k)
+      local a, b = unpack(vim.split(k, '.', true))
+      if b ~= nil then
+        return extensions[a][b]
+      else
+        return builtin[k]
+      end
+    end
+  }
+)
 
 -- Store a map of keymaps to a partial function that calls a picker with specific options.
 -- This way, the picker options tables aren't rebuilt every time the mapping is used.
@@ -34,12 +55,7 @@ function M.map_telescope(lhs, opts)
   if type(opts.picker) == 'function' then
     picker = opts.picker
   elseif type(opts.picker) == 'string' then
-    local a, b = unpack(vim.split(opts.picker, '.', true))
-    if b ~= nil then
-      picker = extensions[a][b]
-    else
-      picker = M[a] or builtin[a]
-    end
+    picker = M[opts.picker]
   else
     util.error('Unsupported type for opts.picker: ', type(opts.picker))
     return
@@ -58,16 +74,102 @@ function M.map_telescope(lhs, opts)
 end
 local map_telescope = M.map_telescope
 
-local function open_in_file_browser(prompt_bufnr)
-  local entry = action_state.get_selected_entry()
-  local dir = from_entry.path(entry)
-  if vim.fn.isdirectory(dir) == 1 then
-    actions.close(prompt_bufnr)
-    builtin.file_browser{cwd = dir}
-  else
-    util.msg('Not a directory: ', dir)
-  end
+-- Pick from neovim configuration files
+function M.nvim_config()
+  builtin.find_files {
+    prompt_title = 'Neovim Configuration',
+    cwd = vim.fn.stdpath('config'),
+    layout_strategy = 'flex',
+    layout_config = {
+      horizontal = {
+        preview_width = 0.6
+      }
+    }
+  }
 end
+
+-- Pick from installed neovim plugins
+function M.nvim_plugins()
+  builtin.find_files {
+    prompt_title = 'Neovim Plugin Files',
+    cwd = as_stdpath('data', 'site/pack/packer'),
+    layout_strategy = 'horizontal',
+    layout_config = {
+      preview_width = 0.6
+    }
+  }
+end
+
+-- Pick from Zsh configuration files
+function M.zsh_config()
+  builtin.find_files {
+    prompt_title = 'Zsh Configuration',
+    cwd = vim.fn.expand('$ZSH'),
+    layout_strategy = 'flex'
+  }
+end
+
+-- Browse ~/Projects
+function M.projects()
+  builtin.file_browser {
+    prompt_title = 'Projects',
+    cwd = '~/Projects'
+  }
+end
+
+-- Pick a directory from z.lua
+function M.z()
+  extensions.z.list {
+    cmd = {vim.o.shell, '-c', string.format('%s %s -l', vim.env.ZLUA_LUAEXE, vim.env.ZLUA_SCRIPT)},
+  }
+end
+
+telescope.setup {
+  defaults = {
+    prompt_prefix = '🔍 ',
+    selection_caret = '❯ ',
+    winblend = 13,
+    mappings = {
+      i = {
+        ['<C-j>'] = 'move_selection_next',
+        ['<C-k>'] = 'move_selection_previous',
+        ['<C-s>'] = 'select_horizontal',
+        ['<C-x>'] = trouble.smart_open_with_trouble,
+        ['<C-b>'] = open_in_file_browser,
+        ["<C-Down>"] = actions.cycle_history_next,
+        ["<C-Up>"] = actions.cycle_history_prev,
+      },
+      n = {
+        ['<C-s>'] = 'select_horizontal',
+        ['<C-x>'] = trouble.smart_open_with_trouble,
+        ['<C-b>'] = open_in_file_browser,
+        ["<C-Down>"] = actions.cycle_history_next,
+        ["<C-Up>"] = actions.cycle_history_prev,
+      }
+    }
+  },
+  pickers = {
+    buffers = {
+      mappings = {
+        i = {
+          ['<M-d>'] = 'delete_buffer'
+        },
+        n = {
+          ['<M-d>'] = 'delete_buffer'
+        }
+      }
+    },
+    search_history = {theme = 'dropdown'},
+    colorscheme = {theme = 'dropdown'},
+    vim_options = {theme = 'dropdown'}
+  },
+  extensions = {
+    fzy_native = {
+      override_generic_sorter = true,
+      override_file_sorter = true,
+    },
+  },
+}
 
 -- Telescope Mappings
 map_telescope('<C-p>',      {'buffers', opts = {sort_mru = true}})
@@ -120,111 +222,4 @@ vim.api.nvim_set_keymap(
   'c', '<C-t>', "getcmdtype() == ':' ? '<Plug>(TelescopeFuzzyCommandSearch)' : '<C-t>'", {expr = true}
 )
 
-telescope.setup {
-  defaults = {
-    prompt_prefix = '🔍 ',
-    selection_caret = '❯ ',
-    winblend = 13,
-    mappings = {
-      i = {
-        ['<C-j>'] = 'move_selection_next',
-        ['<C-k>'] = 'move_selection_previous',
-        ['<C-s>'] = 'select_horizontal',
-        ['<C-x>'] = trouble.smart_open_with_trouble,
-        ['<C-b>'] = open_in_file_browser,
-        ["<C-Down>"] = actions.cycle_history_next,
-        ["<C-Up>"] = actions.cycle_history_prev,
-      },
-      n = {
-        ['<C-s>'] = 'select_horizontal',
-        ['<C-x>'] = trouble.smart_open_with_trouble,
-        ['<C-b>'] = open_in_file_browser,
-        ["<C-Down>"] = actions.cycle_history_next,
-        ["<C-Up>"] = actions.cycle_history_prev,
-      }
-    }
-  },
-  pickers = {
-    buffers = {
-      mappings = {
-        i = {
-          ['<M-d>'] = 'delete_buffer'
-        },
-        n = {
-          ['<M-d>'] = 'delete_buffer'
-        }
-      }
-    },
-    search_history = {theme = 'dropdown'},
-    colorscheme = {theme = 'dropdown'},
-    vim_options = {theme = 'dropdown'}
-  },
-  extensions = {
-    fzy_native = {
-      override_generic_sorter = true,
-      override_file_sorter = true,
-    },
-  },
-}
-
--- Pick from neovim configuration files
-function M.nvim_config()
-  builtin.find_files {
-    prompt_title = 'Neovim Configuration',
-    cwd = vim.fn.stdpath('config'),
-    layout_strategy = 'flex',
-    layout_config = {
-      horizontal = {
-        preview_width = 0.6
-      }
-    }
-  }
-end
-
--- Pick from installed neovim plugins
-function M.nvim_plugins()
-  builtin.find_files {
-    prompt_title = 'Neovim Plugin Files',
-    cwd = as_stdpath('data', 'site/pack/packer'),
-    layout_strategy = 'horizontal',
-    layout_config = {
-      preview_width = 0.6
-    }
-  }
-end
-
--- Pick from Zsh configuration files
-function M.zsh_config()
-  builtin.find_files {
-    prompt_title = 'Zsh Configuration',
-    cwd = vim.fn.expand('$ZSH'),
-    layout_strategy = 'flex'
-  }
-end
-
--- Browse ~/Projects
-function M.projects()
-  builtin.file_browser {
-    prompt_title = 'Projects',
-    cwd = '~/Projects'
-  }
-end
-
--- Pick a directory from z.lua
-function M.z()
-  extensions.z.list {
-    cmd = {vim.o.shell, '-c', string.format('%s %s -l', vim.env.ZLUA_LUAEXE, vim.env.ZLUA_SCRIPT)},
-  }
-end
-
-return setmetatable({}, {
-    __index = function(_, k)
-      if M[k] then
-        return M[k]
-      else
-        return builtin[k]
-      end
-    end
-  }
-)
-
+return M
