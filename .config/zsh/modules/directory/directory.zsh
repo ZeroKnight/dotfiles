fb1b017112d4a4b0b22255355a15ec5c0a02f4ea
ZeroKnight <xzeroknightx@gmail.com>
2021-11-19 19:59:11 -0800

    zsh: Use `exa` and prune `ls` aliases
    Exa is very nice; should have switched sooner. Also, I very rarely used
    the excessive `ls` aliases I made many years ago, and they don't save
    that much typing anyway; chuck 'em in the bin.

--- .config/zsh/modules/directory/directory.zsh
+++ .config/zsh/modules/directory/directory.zsh
@@ -41,29 +41,20 @@ alias pu='pushd'
 alias po='popd'
 alias -- -='cd -'
 
-# ls Shortcuts
-alias ls='ls -hAFH --color=auto --hyperlink=auto --group-directories-first'
-alias ll='ls -l'            # Long-list
-alias lr='ls -R'            # Recursive
-alias lx='ls -BX'           # Sort by Extension, omit backups
-alias lz='ls -S'            # Sort by Size
-alias lt='ls -t'            # Sort by MTime
-alias lc='ls -c'            # Sort by CTime
-alias llr='ll -R'           # Long Recursive
-alias llx='ll -BX'          # Long Sort by Extension
-alias llz='ll -S'           # Long Sort by Size
-alias llt='ll -t'           # Long Sort by MTime
-alias llc='ll -c'           # Long Sort by CTime
+# Directory Listing
+if (( $+commands[exa] )); then
+  export TIME_STYLE='default'  # Maybe switch to long-iso
+  export EXA_ICON_SPACING=2  # Accommodate kitty
 
-# Reverse sort
-alias lX='lx -r'            # Sort by Extension, omit backups, reverse
-alias lZ='lz -r'            # Sort by Size, reverse
-alias lT='lt -r'            # Sort by MTime, reverse
-alias lC='lc -r'            # Sort by CTime, reverse
-alias llX='llx -r'          # Long Sort by Extension, reverse
-alias llZ='llz -r'          # Long Sort by Size, reverse
-alias llT='llt -r'          # Long Sort by MTime, reverse
-alias llC='llc -r'          # Long Sort by CTime, reverse
+  alias exa='exa --group --icons --group-directories-first'
+  alias ls='exa'
+else
+  alias ls='ls -hFH --color=auto --hyperlink=auto --group-directories-first'
+fi
+
+alias ll='ls -l'
+alias lr='ls -R'
+alias llr='ll -R'
 
 alias lss='ls | less'
 alias lls='ll | less'
