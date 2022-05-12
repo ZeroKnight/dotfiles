a300537e9023c419d26aa99802e671cf0eaa962d
ZeroKnight <xzeroknightx@gmail.com>
2022-05-12 00:07:54 -0700

    feat(nvim): Add `<C-w>` mapping to nvim-autopairs

--- .config/nvim/lua/plugin/autopairs.lua
+++ .config/nvim/lua/plugin/autopairs.lua
@@ -5,6 +5,7 @@ local Rule = require 'nvim-autopairs.rule'
 local cond = require 'nvim-autopairs.conds'
 
 npairs.setup {
+  map_c_w = true,
   fast_wrap = {
     map = '<M-w>',
     highlight_grey = 'Visual',
