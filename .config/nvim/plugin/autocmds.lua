9d94af4558075b48e2cf41c5fc232d020c16b48a
ZeroKnight <xzeroknightx@gmail.com>
2022-09-12 01:07:08 -0700

    refactor(nvim): Wrap autocmd function to include group

--- .config/nvim/plugin/autocmds.lua
+++ .config/nvim/plugin/autocmds.lua
@@ -1,14 +1,15 @@
-local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 local opt = vim.opt
 local optl = vim.opt_local
 local optg = vim.opt_global
 
 local g = augroup('ZeroKnight', {})
+local autocmd = function(events, opts)
+  return vim.api.nvim_create_autocmd(events, vim.tbl_extend('force', { group = g }, opts))
+end
 
 autocmd({ 'BufWritePre', 'FileWritePre' }, {
   desc = 'Automatically create directories for new files when saving',
-  group = g,
   callback = function()
     vim.fn.mkdir(vim.fn.expand '<afile>:p:h', 'p')
   end,
@@ -16,14 +17,12 @@ autocmd({ 'BufWritePre', 'FileWritePre' }, {
 
 autocmd('BufWritePost', {
   desc = 'Automatically recompile packer schema on write',
-  group = g,
   pattern = 'lua/zeroknight/plugins.lua',
   command = 'source <afile> | PackerCompile',
 })
 
 autocmd('FileType', {
   desc = 'Many filetype plugins set `formatoptions+=o`. Undo this.',
-  group = g,
   callback = function()
     optl.formatoptions:remove 'o'
   end,
@@ -33,7 +32,6 @@ local relnum_off_events = { 'WinLeave', 'InsertEnter' }
 local relnum_on_events = { 'WinEnter', 'BufWinEnter', 'VimEnter', 'InsertLeave' }
 autocmd(vim.tbl_flatten { relnum_off_events, relnum_on_events }, {
   desc = 'Toggle relative line-numbering in various cases',
-  group = g,
   callback = function(ctx)
     if vim.o.number then
       if vim.tbl_contains(relnum_off_events, ctx.event) then
@@ -47,7 +45,6 @@ autocmd(vim.tbl_flatten { relnum_off_events, relnum_on_events }, {
 
 autocmd({ 'WinEnter', 'WinLeave' }, {
   desc = 'Only show cursor(line|column) in the active window',
-  group = g,
   callback = function(ctx)
     if ctx.event == 'WinLeave' then
       optl.cursorline = false
@@ -61,7 +58,6 @@ autocmd({ 'WinEnter', 'WinLeave' }, {
 
 autocmd('TermOpen', {
   desc = 'Terminal Settings',
-  group = g,
   callback = function()
     for _, o in ipairs { 'number', 'relativenumber', 'cursorline', 'cursorcolumn' } do
       optl[o] = false
@@ -71,7 +67,6 @@ autocmd('TermOpen', {
 
 autocmd('TextYankPost', {
   desc = 'Highlight yanked text',
-  group = g,
   callback = function()
     require('vim.highlight').on_yank { timeout = 300, higroup = 'LspReferenceWrite', on_visual = false }
   end,
@@ -79,6 +74,5 @@ autocmd('TextYankPost', {
 
 autocmd('ColorScheme', {
   desc = 'Redefine highlights on colorscheme change',
-  group = g,
   command = 'silent runtime lua/zeroknight/config/highlight.lua',
 })
