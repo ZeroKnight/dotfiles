d3e9f6540c8151a1a0b637d0d50f8fb1066b26f2
ZeroKnight <xzeroknightx@gmail.com>
2023-04-13 03:16:24 -0700

    refactor(nvim): Overhaul lua_ls configuration
    The baseline configuration for LLS should be minimal and not assume a
    Neovim environment. Loading all of my plugins' lua files into the
    workspace library was entirely unnecessary and caused a ton of
    "latency". This was mostly due to misunderstandings and half-efforts on
    my part at the time of initial setup.
    
    We now leverage lspconfig's `on_new_config` callback to only load in the
    Neovim specific stuff when we're actually editing something from our
    config (fancy that). Additionally, we now ensure that the `root_dir` is
    always set to the nvim config dir instead of sometimes being rooted at
    the repo root depending on the file edited.

--- .config/nvim/lua/zeroknight/lsp/servers/lua_ls.lua
+++ .config/nvim/lua/zeroknight/lsp/servers/lua_ls.lua
@@ -1,6 +1,8 @@
--- Sumneko Lua LSP Config
+-- Lua Language Server Config
 
-local function lua_path()
+local uv = vim.loop
+
+local function nvim_lua_path()
   local p = vim.split(package.path, ';')
   table.insert(p, 'lua/?.lua')
   table.insert(p, 'lua/?/init.lua')
@@ -13,35 +15,57 @@ M.config = {
   settings = {
     Lua = {
       runtime = {
-        version = 'LuaJIT',
-        path = lua_path(),
+        version = 'Lua 5.2',
       },
       completion = {
         enable = true,
         autoRequire = true,
         callSnippet = 'Disable',
         keywordSnippet = 'Disable', -- Use regular snippets for this
-        displayContext = 6,         -- Show function lines in suggestion
+        displayContext = 6, -- Show function lines in suggestion
         showParams = true,
         workspaceWord = true,
       },
       diagnostics = {
         enable = true,
-        globals = { 'vim', 'packer_plugins' },
         -- Disabled diagnostic categories
         disable = { 'undefined-field', 'undefined-global' },
+        libraryFiles = 'Disable',
+      },
+      format = {
+        -- Using stylua via null-ls
+        enable = false,
       },
       semantic = {
-        enable = false, -- No plans to use Semantic highlighting
+        -- TODO: test if treesitter is doing semantic highlighting for lua
+        enable = false,
       },
-      workspace = {
-        -- Include Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true),
-      },
+      workspace = {},
       -- Sumneko's telemetry looks minimal and non-invasive.
       telemetry = { enable = true },
     },
   },
+  on_new_config = function(new_config, new_root_dir)
+    -- Editing Neovim configuration
+    if new_root_dir == uv.fs_realpath(vim.fn.stdpath 'config') then
+      new_config.root_dir = function(_)
+        return vim.fn.stdpath 'config'
+      end
+      new_config.settings.Lua = vim.tbl_deep_extend('force', new_config.settings.Lua, {
+        runtime = {
+          version = 'LuaJIT',
+          path = nvim_lua_path(),
+        },
+        diagnostics = {
+          globals = { 'vim', 'packer_plugins' },
+        },
+        workspace = {
+          -- Include Neovim runtime files
+          library = vim.api.nvim_get_runtime_file('', true),
+        },
+      })
+    end
+  end,
 }
 
 return M
