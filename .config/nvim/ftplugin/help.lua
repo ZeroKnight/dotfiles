108aa8cc9d47d08e1a2a882682a9bf375cdd5b25
ZeroKnight <xzeroknightx@gmail.com>
2023-04-13 23:45:22 -0700

    refactor(nvim): Revert old quit shortcut change for help

--- .config/nvim/ftplugin/help.lua
+++ .config/nvim/ftplugin/help.lua
@@ -1,5 +1,5 @@
 -- Help filetype settings
 
-vim.keymap.set('', 'gq', '<Cmd>q<CR>', { buffer = true, silent = true })
+vim.keymap.set('', 'q', '<Cmd>q!<CR>', { buffer = true, silent = true })
 vim.keymap.set('n', '<CR>', '<C-]>', { buffer = true })
 vim.keymap.set('n', '<BS>', '<C-T>', { buffer = true })
