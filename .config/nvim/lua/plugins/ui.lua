e835a9c87110108fbd79714dcbcc5db578f40d81
ZeroKnight <xzeroknightx@gmail.com>
2025-05-07 15:39:29 -0700

    chore(nvim): Remove forgotten nvim-notify dependency

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -301,7 +301,6 @@ return {
     'folke/noice.nvim',
     event = 'VeryLazy',
     dependencies = {
-      'rcarriga/nvim-notify',
       { 'MunifTanjim/nui.nvim', lazy = true },
     },
     opts = {
