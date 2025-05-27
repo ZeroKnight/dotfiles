71de991d889e29b9baeaa8de30792a38c8f9a01c
ZeroKnight <xzeroknightx@gmail.com>
2025-05-26 19:26:56 -0700

    refactor(nvim): Remove vim-scriptease
    I don't really work with vimscript anymore and don't have any immediate
    plans to. Plus, Neovim has a `g==` mapping now.

--- .config/nvim/lua/plugins/debug.lua
+++ .config/nvim/lua/plugins/debug.lua
@@ -16,20 +16,4 @@ return {
     dependencies = { 'mfussenegger/nvim-dap' },
     enabled = false,
   },
-
-  -- Vim
-  {
-    'tpope/vim-scriptease',
-    ft = 'vim',
-    keys = {
-      { 'zS', desc = 'Show syntax highlighting groups under cursor' },
-      { 'g=', desc = 'eval() text and replace it with the result', mode = { 'n', 'x' } },
-      { 'g==', desc = 'eval() line and replace it with the result' },
-    },
-      -- stylua: ignore
-      cmd = {
-        'Breakadd', 'Breakdel', 'Disarm', 'Messages', 'Runtime', 'Scriptnames', 'Time', 'Verbose',
-        'Vedit', 'Vopen', 'Vread', 'Vsplit', 'Vvsplit', 'Vtabedit', 'Vpedit',
-      },
-  },
 }
