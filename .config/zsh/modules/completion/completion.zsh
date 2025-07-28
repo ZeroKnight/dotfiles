793c2206bc2ced435eb22a30dcf96084246985ce
ZeroKnight <xzeroknightx@gmail.com>
2025-07-27 21:25:27 -0700

    refactor(zsh): Update completion/completion.zsh
    After many years of using this completion setup untouched, I decided to
    revisit it and tweak some things on a whim. There's a lot of cosmetic
    changes to the tune or reorganizing, but there are some actual changes
    as well:
    
    - Added `_prefix` completer for `_complete`
    - Added `show-completer` style
    - Set `accept-exact` to false for normal completion
    - Skip menu selection for single-error unambiguous corrections
    - Expand will also list completable parameter names if any in addition
      to expansions
    - Results from the `_match` completer have their own formatting
    - The formatting for `_approximate` and `_correct` now actually work
        - I mixed up the completer and tags fields in the context string...
          whoops!
    - Show completions for `_`-prefixed functions only when we've typed an
      `_` (meaning we actually want them) instead of ignoring them
      completely
      - We were using the `_ignored` completer before, so this behaves in
        a slightly different but significant way

--- .config/zsh/modules/completion/completion.zsh
+++ .config/zsh/modules/completion/completion.zsh
@@ -8,64 +8,120 @@ autoload -U is-at-least
 # Load extra compdefs
 source ${0:h}/compdefs.zsh
 
-### Zsh completion options
+# Zsh completion options
 
-setopt always_to_end
+setopt always_to_end complete_in_word
 setopt auto_list auto_menu no_menu_complete
-setopt auto_param_slash
-setopt auto_param_keys
-setopt complete_in_word
+setopt auto_param_slash auto_param_keys auto_remove_slash
 
 setopt case_glob case_match
 setopt extended_glob
 
 is-at-least '5.2' && setopt glob_star_short
 
-### zstyles - Fine tune completion
+#
+# zstyles - Fine tune completion
+#
 
-# General Completer settings
-zstyle ':completion:*' completer _extensions _expand _complete _ignored _match _approximate
-zstyle ':completion::approximate:*' max-errors 3 numeric
-zstyle ':completion::(match|approximate):*' insert-unambiguous true
-zstyle ':completion::expand:*' accept-exact true
-zstyle ':completion::expand:*' add-space file subst
-zstyle ':completion::expand:*' group-name ''
-zstyle ':completion::expand:*' group-order all-expansions expansions original
-zstyle ':completion:*:*:*:*:*' menu select
-zstyle ':completion:*:*:*:*:*' single-ignored menu
-zstyle ':completion::complete:*' use-cache true
-zstyle ':completion::complete:*' cache-path "$ZCACHEDIR/zcompcache"
+# NOTE: zstyle always prefers to match the most specific context pattern.
+#
+# A context pattern is considered "more specific" than another pattern when:
+#   1. It contains more fields
+#   2. The component patterns in each field are more specific
+#
+# Component patterns are more specific than another when:
+#   1. It is a simple string
+#   2. It is a complex pattern
+#   3. It is a single `*` wildcard
+#
+# See also the `zstyle` section of zshmodules(1), zshcompsys(1), zshguide06, and
+# the Completion-System doc.
+
+# NOTE: I could be wrong, but I don't believe an empty field is the same as a
+# lone wildcard. A completer may not provide a value for a given field in all
+# contexts, like _approximate.
+
+### General Completer settings
+
+# NOTE: Do not bind `expand-or-complete` when using the _expand completer
+zstyle ':completion:*' completer _extensions _expand _complete _prefix _ignored _match _approximate
+zstyle ':completion:*' menu select
+zstyle ':completion:*' single-ignored menu
+zstyle ':completion:*' show-completer true
 
 # Matcher - Case insensitive, then partial-word completion, then substring
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 
-# Formatting
+### Complete
+
+zstyle ':completion::complete:*' use-cache true
+zstyle ':completion::complete:*' cache-path "$ZCACHEDIR/zcompcache"
+
+# Don't auto-accept ambiguous matches that happened to be exactly correct
+zstyle ':completion::complete:*' accept-exact false
+
+### Approximate
+
+# Skip menu selection if the correction is unambiguous with only a single error
+zstyle ':completion::approximate:*' max-errors 3 numeric
+zstyle ':completion::(approximate|correct)-1:*' insert-unambiguous true
+zstyle ':completion::(approximate|correct|match):*' insert-unambiguous false
+
+### Expand
+
+zstyle ':completion::expand:*' add-space file subst
+
+# Allow both completion and expansion of parameter names
+#
+# The _expand completer allows this via `accept-exact continue`, which will
+# continue to the next completer (should be _complete). Since two completers
+# are invoked, getting the group naming and contexts right is tricky: we have
+# set `group-name` for parameters in the context of _complete.
+zstyle ':completion::expand:*' accept-exact continue
+zstyle ':completion::expand:*' group-name ''
+zstyle ':completion::complete:-parameter-:*' group-name ''
+zstyle ':completion::expand:*' group-order parameters all-expansions expansions original
+
+### Prefix
+
+zstyle ':completion::prefix:*' add-space true
+
+### Formatting
+
 zstyle ':completion:*:options' description true
 zstyle ':completion:*:options' auto-description '%d'
 zstyle ':completion:*' list-separator '|'
+
 zstyle ':completion:*' format ' %F{green}> > %d%f'
+zstyle ':completion::match:*' format ' %F{blue}> > matches for %d%f'
+zstyle ':completion::match:*:original' format ' %F{green}> > %d%f'
 zstyle ':completion:*:messages' format ' %F{magenta}> > %d%f'
 zstyle ':completion:*:warnings' format ' %F{red}> > no matches found!%f'
-zstyle ':completion:*:(approximate|correct)' format ' %F{yellow}> > %d for %B%o%b (errors: %e)%f'
-zstyle ':completion:*:*expansions' format ' %F{cyan}> > %d for %B%o%b%f'
+zstyle ':completion:*:corrections' format ' %F{yellow}> > %d for %B%o%b (errors: %e)%f'
+zstyle ':completion:*:(all-)?expansions' format ' %F{cyan}> > %d for %B%o%b%f'
 zstyle ':completion:*:default' list-prompt '%S%m matches | line %l | %p%s'
 zstyle ':completion:*:default' select-prompt '%S%m matches | line %l | %p%s'
 
+### Result Filtering
+
 # Ignored Patterns
 zstyle ':completion:*:*:(^rm):*:*files' ignored-patterns '*?.o' '*?.zwc' 'tags'
-zstyle ':completion:*:functions' ignored-patterns '_*'
 zstyle ':completion:*:users' ignored-patterns avahi bin colord daemon dbus ftp git 'gpm(-root)?' http mail nobody polkitd sshd 'systemd-*' uuidd
 
 # Ignore duplicate elements
 zstyle ':completion:*:(rm|kill|*diff):*' ignore-line other
 
-# Behavior
-# Group builtins with external commands to condense
+# Only complete prefixed functions when a `_` or `.` is explicitly given
+zstyle ':completion:*:functions' prefix-needed true
+
+### Context-specific Behaviors
+
+# Group builtins with external commands to condense output
 zstyle ':completion:*:*:-command-:*:*' group-name ''
 zstyle ':completion:*:*:-command-:*:builtins' group-name 'commands'
 zstyle ':completion:*:*:-command-:*:*' group-order functions aliases commands
 
-# Only complete parameters we're completing at a $ in a subscript
+# Only complete parameters when we're completing at a $ in a subscript
 zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
 
 # Directories
