8d2e2d2b9e845835e7cebbc805881ccca349697f
ZeroKnight <xzeroknightx@gmail.com>
2025-10-15 00:00:01 -0700

    feat(nvim): Add Quadlet filetype detection
    Consider submitting a PR for this.

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -1,7 +1,16 @@
 -- Additional filetype detection
 
 vim.filetype.add {
-  extension = {
-    t = 'perl', -- Perl test files
+  -- NOTE: filetype.add does some implicit anchoring of these patterns
+  pattern = {
+    -- Podman Quadlet Units
+    ['.*/containers/systemd/.*%.container'] = 'systemd',
+    ['.*/containers/systemd/.*%.volume'] = 'systemd',
+    ['.*/containers/systemd/.*%.network'] = 'systemd',
+    ['.*/containers/systemd/.*%.kube'] = 'systemd',
+    ['.*/containers/systemd/.*%.image'] = 'systemd',
+    ['.*/containers/systemd/.*%.build'] = 'systemd',
+    ['.*/containers/systemd/.*%.pod'] = 'systemd',
+    ['.*/containers/systemd/.*%.d/.*%.conf'] = 'systemd',
   },
 }
