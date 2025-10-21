ZeroKnight/dotfiles

commit a2cbc37b4ba73becb0b4179fca2dc43b9734b61a

fix(nvim): Remove Comment.nvim pre hook
No longer using context commentstring.

.config/nvim/lua/plugins/editing.lua
@@ -155,9 +155,6 @@ return {
     keys = {
       { 'gc' }, { 'gc', mode = 'v' }, { 'gb' }, { 'gb', mode = 'v' }, { 'gcc' }, { 'gco' }, { 'gcO' }, { 'gcA' }
     },
-    opts = {
-      pre_hook = function() require('ts_context_commentstring.internal').update_commentstring {} end,
-    },
   },
 
   {

commit 5188f4c36c26493144b1574b7ec9e57b3fb9a181

fix(nvim): Comment.nvim not loading
Removing the `opts` key in commit a2cbc37 caused Lazy to no longer
`require` the plugin.

.config/nvim/lua/plugins/editing.lua
@@ -151,9 +151,18 @@ return {
 
   {
     'numToStr/Comment.nvim',
-    -- stylua: ignore
+    opts = {
+      padding = true,
+      sticky = true,
+      mappings = { basic = true, extra = true },
+    },
     keys = {
-      { 'gc' }, { 'gc', mode = 'v' }, { 'gb' }, { 'gb', mode = 'v' }, { 'gcc' }, { 'gco' }, { 'gcO' }, { 'gcA' }
+      { 'gc', mode = { 'n', 'v' } },
+      { 'gb', mode = { 'n', 'v' } },
+      { 'gcc' },
+      { 'gco' },
+      { 'gcO' },
+      { 'gcA' },
     },
   },
 