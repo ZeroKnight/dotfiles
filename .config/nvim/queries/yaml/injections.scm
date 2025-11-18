ca08ad21add63c8b59324f61aa8476d1fad8ad79
ZeroKnight <xzeroknightx@gmail.com>
2025-11-17 23:20:31 -0800

    feat(nvim): improve Ansible Jinja injections
    More consistent injection for the variety of string scalars available,
    as well as a hacky way to highlight `*when` directives.

--- .config/nvim/queries/yaml/injections.scm
+++ .config/nvim/queries/yaml/injections.scm
@@ -1,12 +1,25 @@
 ;; extends
 
-(block_mapping_pair
-  value: (flow_node
-    (double_quote_scalar) @injection.content
-    (#set! injection.language "jinja")
-    (#offset! @injection.content 0 1 0 -1)))
-    (#lua-match? @injection.content "^{{.+}}$")
+(flow_node
+  [(double_quote_scalar) (single_quote_scalar)] @injection.content
+  (#set! injection.language "jinja")
+  (#offset! @injection.content 0 1 0 -1)
+  (#lua-match? @injection.content "{{.+}}"))
 
+(flow_node
+  (plain_scalar
+    (string_scalar) @injection.content
+    (#set! injection.language "jinja")
+    (#lua-match? @injection.content "{{.+}}")))
+
+(block_node
+  (block_scalar) @injection.content
+  (#set! injection.language "jinja")
+  (#lua-match? @injection.content "{{.+}}"))
+
+; FIXME: Correct capture, but no highlighting due to a lack of brace pairs
+; We can cheat a bit by setting the injected language to Python; it's not
+; totally accurate, but it's close enough.
 (block_mapping_pair
   key: (flow_node
     (plain_scalar
@@ -15,4 +28,4 @@
   value: (flow_node
     (plain_scalar
       (string_scalar) @injection.content
-      (#set! injection.language "jinja"))))
+      (#set! injection.language "python")))) ; HACK: *Should* be "jinja"
