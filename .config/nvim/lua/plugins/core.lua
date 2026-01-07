852bf3129e2a6938284675f88cc1a63ba31c4db6
ZeroKnight <xzeroknightx@gmail.com>
2026-01-07 00:05:53 -0800

    chore(nvim): minor plugin reorganization
    I made `plugins/core.lua` way back when I originally migrated to
    lazy.nvim with a vague idea of what else might go there, only to never
    get around to putting anything else there.
    
    To make the spec for lazy less lonely, I've moved some low-level,
    "library"-esque plugins from utility to core.

--- .config/nvim/lua/plugins/core.lua
+++ .config/nvim/lua/plugins/core.lua
@@ -3,4 +3,7 @@
 ---@type LazySpec
 return {
   { 'folke/lazy.nvim', version = '*', branch = 'main' },
+  { 'nvim-lua/plenary.nvim', lazy = true },
+  { 'tpope/vim-repeat', event = 'VeryLazy' },
+  { 'nvim-lua/popup.nvim', lazy = true },
 }
