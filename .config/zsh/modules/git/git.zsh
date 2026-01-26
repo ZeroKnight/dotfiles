38b8d158c6a5d9b3d3e3a14444c0d54cb7338416
ZeroKnight <xzeroknightx@gmail.com>
2026-01-26 15:25:36 -0800

    refactor(git): use git-config options over aliases

--- .config/zsh/modules/git/git.zsh
+++ .config/zsh/modules/git/git.zsh
@@ -6,8 +6,9 @@
 ### Settings
 
 # Diff
-_git_diff_normal_format='--patch-with-stat --dirstat'
-_git_diff_word_format="$_git_diff_normal_format --word-diff=color"
+_git_diff_format_stat='--stat --dirstat'
+_git_diff_format_pstat='--patch-with-stat --dirstat'
+_git_diff_format_word="$_git_diff_normal_format --word-diff=color"
 
 ### Aliases
 
@@ -25,26 +26,22 @@ alias gm='git merge'
 alias ga='git add'
 alias gA='git add --patch'
 alias gb='git branch'
-alias gc='git commit --verbose'
+alias gc='git commit'
 
 # Diff
-alias gd='git diff $=_git_diff_normal_format'
-alias gD='git diff --cached $=_git_diff_normal_format'
-alias gdw='git diff $=_git_diff_word_format'
-alias gDw='git diff --cached $=_git_diff_word_format'
+alias gd='git diff $=_git_diff_format_pstat'
+alias gD='git diff --cached $=_git_diff_format_pstat'
+alias gdw='git diff $=_git_diff_format_word'
+alias gDw='git diff --cached $=_git_diff_format_word'
 
 # Log
 alias gl='git log --all --graph --pretty=single'
-alias glv='git log --topo-order --stat --dirstat --pretty=fullinfo'
+alias glv='git log --topo-order --pretty=fullinfo $=_git_diff_format_stat'
 
 # Stash
 alias gs='git stash'
-alias gsa='git stash apply'
-alias gsb='git stash branch'
-alias gsx='git stash drop'
+alias gS='git stash --patch'
 alias gsl='git stash list'
-alias gsd='git stash show $_git_diff_normal_format'
-alias gsdw='git stash show $_git_diff_word_format'
+alias gsd='git stash show'
+alias gsdw='git stash show $=_git_diff_format_word'
 alias gsp='git stash pop'
-alias gss='git stash save'
-alias gsS='git stash save --patch'
