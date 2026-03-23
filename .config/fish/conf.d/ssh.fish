7b1ef9686a452b8dbac5b621afe090dedafe20cf
ZeroKnight <xzeroknightx@gmail.com>
2026-03-22 21:39:34 -0700

    fix(fish): ensure ASKPASS variables are set before running keychain
    I'm not sure why I had the order reversed in the first place, but
    beside the point, keychain was failing in an interesting way. Because
    I'm also setting ASKPASS variables in environment.d, keychain was
    still picking up on them... but `SSH_ASKPASS` is set to
    `/usr/bin/ssh-askpass`, which doesn't exist on my Tumbleweed system. So
    keychain barfed.
    
    I assumed at the time that installing any valid askpass implementation
    would create a link or something here, but that isn't the case for
    Tumbleweed. Indeed, I even manually made a link for this...but in
    `/usr/local/bin`. I'm not sure if I want to keep the environment.d file
    since I don't have much choice but to hard-code an ssh-askpass path, but I might be relying on it on other systems (maybe), so I'll keep it for now.
    
    At the very least, I can make sure they're set correctly in interactive
    sessions on fish.

--- .config/fish/conf.d/ssh.fish
+++ .config/fish/conf.d/ssh.fish
@@ -3,14 +3,16 @@
 #
 
 if status is-interactive
+    if command -q ssh-askpass
+        set -gx SSH_ASKPASS_REQUIRE prefer
+        set -gx SSH_ASKPASS (command -s ssh-askpass)
+        set -gx GIT_ASKPASS $SSH_ASKPASS
+    end
+
     if command -q keychain
         keychain --eval --quiet --quick \
             --attempts 3 --agents ssh \
             --absolute --dir $HOME/.local/state/keychain \
             $HOME/.ssh/*.key | source
-    end
-
-    if command -q ssh-askpass
-        set -gx SSH_ASKPASS (command -s ssh-askpass)
     end
 end
