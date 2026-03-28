c1f8de70b6e941c55411dc036483104b0ca18f55
ZeroKnight <xzeroknightx@gmail.com>
2026-03-27 23:50:43 -0700

    refactor(fish): update name and description of bat/delta handler

--- .config/fish/conf.d/bat-delta.fish
+++ .config/fish/conf.d/bat-delta.fish
@@ -3,7 +3,7 @@
 #
 
 # Since Delta uses BAT_THEME, we get automatic theme selection for it as well
-function _update_bat_theme --description 'Set BAT_THEME based on $fish_terminal_color_theme' --on-variable fish_terminal_color_theme
+function _update_bat_delta_theme -d 'Set BAT_THEME and DELTA_FEATURES based on $fish_terminal_color_theme' -v fish_terminal_color_theme
     switch $fish_terminal_color_theme
         case dark
             set -gx BAT_THEME tokyonight_$tokyonight_variant
