0f687333e7bf6ce355769615c15eede58af28160
ZeroKnight <xzeroknightx@gmail.com>
2026-03-27 23:08:08 -0700

    feat(delta): use git's diff.colorMoved feature
    I had been putting off enabling this feature because I was still trying
    to figure out a way to handle using the correct day/night colors as
    needed, as again, Delta does not yet support automatic day/night theme
    switching.
    
    However, I found that Delta *does* support setting features on the fly
    via the `DELTA_FEATURES` environment variable! So I can create delta
    features with the correct `colorMoved` colors and select the appropriate
    one the same way I select the bat theme.
    
    For reference, I got the "dimmed" `colorMoved` colors by blending the
    **blue** and **magenta** values for Storm and Day with their respective
    background colors. I threw them into https://colorkit.io and chose two
    steps up from the background color (with the default 7 steps).

--- .config/fish/conf.d/bat-delta.fish
+++ .config/fish/conf.d/bat-delta.fish
@@ -1,5 +1,5 @@
 #
-# bat configuration
+# bat and Delta configuration
 #
 
 # Since Delta uses BAT_THEME, we get automatic theme selection for it as well
@@ -7,7 +7,9 @@ function _update_bat_theme --description 'Set BAT_THEME based on $fish_terminal_
     switch $fish_terminal_color_theme
         case dark
             set -gx BAT_THEME tokyonight_$tokyonight_variant
+            set -gx DELTA_FEATURES +tokyonight_$tokyonight_variant
         case light
             set -gx BAT_THEME tokyonight_day
+            set -gx DELTA_FEATURES +tokyonight_day
     end
 end
