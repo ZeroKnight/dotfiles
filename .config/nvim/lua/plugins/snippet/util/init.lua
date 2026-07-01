aa6db6f2094e8b4eba03c616b796e6ed5ab8bad0
ZeroKnight <xzeroknightx@gmail.com>
2026-07-01 00:06:33 -0700

    feat(nvim): update luasnip utils
    - Added proper lua_ls annotations to most functions
    - Reworked `maybe_surround`
      - I'm not sure why I wrote it the way that I did in the first place
      - The new version is much more flexible, if a bit clunky

--- .config/nvim/lua/plugins/snippet/util/init.lua
+++ .config/nvim/lua/plugins/snippet/util/init.lua
@@ -8,6 +8,9 @@ local M = {}
 
 -- Snippet data is sometimes within a parent snippet (i.e. snippetnode), so
 -- try each parent until the data we want is found.
+---@param snip LuaSnip.Snippet|LuaSnip.Node
+---@param field string the field to access on the parent
+---@return any
 function M.get_snip(snip, field)
   while snip[field] == nil do
     snip = snip.parent
@@ -39,6 +42,11 @@ end
 --
 -- The opts table expects either a condition (`cond`), an optional `priority`,
 -- or both.
+---@param opts { cond: (fun(): boolean), priority: integer } persistent options passed to the wrapped `ls.s`
+--- - cond: a predicate that determines whether snippets defined by this wrapper
+---   should be expanded
+--- - priority: implicit priority of all snippets defined by this wrapper
+---@return fun(ctx, nodes, s_opts): LuaSnip.Snippet
 function M.snippet_subtype(opts)
   return function(ctx, nodes, s_opts)
     if type(ctx) == 'string' then
@@ -82,6 +90,10 @@ function M.get_comment()
   end
 end
 
+-- Return the requested fold marker based on the value of `'foldmarker'`, or
+-- if unspecified, the literal value of `'foldmarker'`.
+---@param marker 'open'|'closed'|nil which marker to return
+---@return string
 function M.get_foldmarker(marker)
   ---@diagnostic disable-next-line: redundant-parameter
   vim.validate(
@@ -100,7 +112,9 @@ function M.get_foldmarker(marker)
   end
 end
 
--- Return a text node with `n` newlines. Default is one.
+-- Return a text node with `n` newlines; shorthand for `ls.t {'', '', ...}`.
+---@param n integer number of newlines to return (default: 1)
+---@return LuaSnip.Node
 function M.nl(n)
   local nodes = { '' }
   for _ = 1, n or 1 do
@@ -109,32 +123,54 @@ function M.nl(n)
   return ls.t(nodes)
 end
 
--- Return a function node that expands to `text` surrounded by `opening` and
--- `closing` only if the jumpable node at `pos` is not empty. Otherwise,
--- expands to an empty string.
-function M.maybe_surround(pos, text, opening, closing)
-  return extras.nonempty(pos, string.format('%s%s%s', opening, text, closing))
+-- Return a function for a dynamic node with a node reference. The function
+-- will conditionally surround the node returned by `node_fn` with text when the
+-- node referenced by the dynamic node is not empty.
+--
+-- The node to wrap (which could be a SnippetNode) needs to be returned by
+-- `node_fn` since node creation is context-sensitive.
+---@param opening string left side of the surrounding text
+---@param closing string right side of the surrounding text
+---@param node_fn fun(): LuaSnip.Node a function that returns the node to surround
+---@return fun(args: string[]): LuaSnip.SnippetNode cb dynamic node callback
+function M.maybe_surround(opening, closing, node_fn)
+  return function(args)
+    local has_text = #args > 1 or args[1][1] ~= ''
+    return ls.sn(nil, {
+      ls.t(has_text and opening or ''),
+      node_fn(),
+      ls.t(has_text and closing or ''),
+    })
+  end
 end
 
--- Return a function node that expands to `if_match` when the snippet trigger
--- used matches `pat`, or `fallback` otherwise, which defaults to nothing.
--- If `exact` is `true` then the trigger must exactly match `pat`.
--- Defaults to `false`.
-function M.if_trigger(pat, text, fallback, exact)
+-- Return a function node that expands to `text` when the snippet trigger
+-- used matches `pat`, or `fallback` otherwise (which defaults to nothing).
+---@param pat string pattern to match against snippet trigger
+---@param text string node result when matched
+---@param fallback string? node result when not matched (default: '')
+---@param opts { exact: boolean, node_opts: LuaSnip.Opts.Node } optional parameters
+--- - exact: match `pat` literally instead
+--- - node_opts: options to pass to the function node, e.g. `key`
+---@return LuaSnip.Node f_node
+function M.if_trigger(pat, text, fallback, opts)
+  opts = opts or {}
   return ls.f(function(_, snip)
     local trigger = M.get_snip(snip, 'trigger')
-    if exact then
+    if opts.exact then
       return trigger == pat and text or fallback or ''
     else
       return trigger:match(pat) and text or fallback or ''
     end
-  end)
+  end, nil, opts.node_opts or nil)
 end
 
--- Function node whose expansion is determined by a map of patterns and
--- potential result strings. Each pattern in `map` will be used to match
--- against the snippet trigger, and if one matches its associated string
--- will be returned. If nothing matches, an empty string is expanded.
+-- Return a function node whose expansion is determined by a map of patterns and
+-- potential result strings. Each pattern in `map` is compared against the
+-- snippet trigger, expanding to the associated string if it matches. If no
+-- patterns match, an empty string is expanded instead.
+---@param map table<string, string> a map of patterns to result strings
+---@return LuaSnip.Node f_node
 function M.when_trigger(map)
   return ls.f(function(_, snip)
     for pat, output in pairs(map) do
@@ -147,6 +183,13 @@ function M.when_trigger(map)
   end)
 end
 
+-- Return a function node whose expansion is determined by a map of patterns and
+-- potential result strings. Each pattern in `map` is compared against the `n`th
+-- capture of the snippet trigger, expanding to the associated string if it
+-- matches. If no patterns match, an empty string is expanded instead.
+---@param n integer the `n`th capture group in the snippet trigger
+---@param map table<string, string> a map of patterns to result strings
+---@return LuaSnip.Node f_node
 function M.when_capture(n, map)
   return ls.f(function(_, snip)
     local capture = M.get_snip(snip, 'captures')[n]
@@ -162,6 +205,10 @@ end
 -- Return a dynamic node that expands to the desired snippet selection text.
 -- If the selection is empty, return `fallback_text` or an empty string if
 -- unspecified.
+---@param pos integer jump index for the created insert node
+---@param selection_type string snippet variable like `TM_*`, `SELECT_DEDENT`, etc
+---@param fallback_text string? fallback text when there was no selection
+---@return LuaSnip.Node d_node
 function M.selection(pos, selection_type, fallback_text)
   return ls.d(pos, function(_, snip)
     local sel = M.get_snip(snip, 'env')[selection_type]
@@ -173,12 +220,16 @@ function M.selection(pos, selection_type, fallback_text)
   end)
 end
 
--- Return a function for a dynamic node that generates a number of delimited
--- insert nodes equivalent to how many times `pattern` matches within the
--- node specified by the dynamic node. Useful for generating argument lists,
--- docstrings, etc. `opts` can consist of the following:
---    * delimiter [string]: Used to delimit each insert node (Default: `', '`)
---    * regex [bool]: Interpret `pattern` as a Vim pattern (Default: false)
+-- Return a function for a dynamic node with a node reference. Generates
+-- a number of delimited insert nodes equivalent to how many times `pattern`
+-- matches within the node referenced by the dynamic node.
+--
+-- Useful for generating argument lists, docstrings, etc.
+---@param pattern string pattern to match against the reference node
+---@param opts { delimiter: string, regex: boolean } optional parameters
+--- - delimiter: Used to delimit each insert node (Default: `', '`)
+--- - regex: Interpret `pattern` as a Vim pattern (Default: `false`)
+---@return fun(args: string[]): LuaSnip.SnippetNode cb dynamic node callback
 function M.generate_insertnodes(pattern, opts)
   opts = vim.tbl_extend('keep', opts or {}, { delimiter = ', ', regex = false })
   -- Iterator that searches with either Vim or Lua patterns
